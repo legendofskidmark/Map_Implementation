86dd077519cfdfadfbe93fc78969dbf1532bab4f
boon <aaron25rony@gmail.com>
2023-02-02 05:31:56 -0400

    fixed bugs

--- src/CSCI/SDC3901/Assignment1/A1.java
+++ src/CSCI/SDC3901/Assignment1/A1.java
@@ -2,49 +2,49 @@ package CSCI.SDC3901.Assignment1;
 
 
 import java.io.*;
+import java.util.Scanner;
 
 public class A1 {
     public static void main(String[] args) {
-        RecipeBook r = new RecipeBook();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter unit conversion file name : ");
+        String unitConversionFileName = scanner.next();
+
+        System.out.println("Enter recipe content's file name : ");
+        String recipeFileName = scanner.next();
+
+        RecipeBook recipeBook = new RecipeBook();
 
         FileReader frUC;
         FileReader frRecipe;
         try {
-            frUC = new FileReader("unitConversion.txt");
-            frRecipe = new FileReader("recipe.txt");
+            frUC = new FileReader(unitConversionFileName);
+            frRecipe = new FileReader(recipeFileName);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
         BufferedReader brUC = new BufferedReader(frUC);
         BufferedReader brR = new BufferedReader(frRecipe);
-        r.unitConversion(brUC); //first file
-
-        try {
-            frUC = new FileReader("unitConversion2.txt");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        brUC = new BufferedReader(frUC);
-        r.unitConversion(brUC); //second file
+        recipeBook.unitConversion(brUC); //first file
 
         PrintWriter pw;
         try {
-            File file = new File("opboon.txt");
+            File file = new File("output.txt");
 
             if (!file.exists()) {
                 file.createNewFile();
             }
 
             pw = new PrintWriter(file);
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
 
-        r.recipe("Metric", brR);
-        r.convert("Egg Curry", "metric", 2, pw);
-
+        recipeBook.recipe("Metric", brR);
+        recipeBook.convert("Egg Curry", "imperial", 2, pw);
 
         pw.close();
 
